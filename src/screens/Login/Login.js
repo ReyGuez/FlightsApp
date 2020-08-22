f5b8998f2116c074d4320e529a4e3d03fd537126
Jesus <Jesusreynaga96@gmail.com>
2020-08-21 21:13:23 -0500

    Add logo

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -8,9 +8,11 @@ import {
   SafeAreaView,
   StatusBar,
   Image,
+  Dimensions,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import i18n from '../../translations/il18';
+const {width} = Dimensions.get('screen');
 
 export default class Login extends Component {
   render() {
@@ -28,9 +30,23 @@ export default class Login extends Component {
             barStyle="light-content"
           />
           <View style={styles.root}>
-            <View style={{flex: 0.2}} >
-              <Image source = {require('../../../assets/logos/Vuelos.png')}
-              style = {{height:50, width:50}} />
+            <View style={{flex: 0.2}}>
+              <View
+                style={{
+                  height: width < 330 ? 80 : 100,
+                  width: width < 330 ? 80 : 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  alignSelf: 'center',
+                  backgroundColor: 'rgba(247, 247, 247, 0.9)',
+                  borderRadius: 50,
+                  marginTop: width < 330 ? 10 : 0,
+                }}>
+                <Image
+                  source={require('../../../assets/logos/Vuelos.png')}
+                  style={{height: width < 330 ? 65 : 80, width:  width < 330 ? 65 :80}}
+                />
+              </View>
             </View>
             <View style={{flex: 1}}>
               <Text style={styles.advice}>{i18n.t('login.welcome')}</Text>
@@ -63,7 +79,9 @@ export default class Login extends Component {
                   <Text style={styles.labelBtn}>{i18n.t('login.signIn')}</Text>
                 </TouchableOpacity>
                 <View style={styles.line} />
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('NewUser')}  style={[styles.btnLogin, {width: '60%'}]}>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate('NewUser')}
+                  style={[styles.btnLogin, {width: '60%'}]}>
                   <Text style={styles.labelBtn}>
                     {i18n.t('login.register')}
                   </Text>
--- src/screens/NewUser/NewUser.js
+++ src/screens/NewUser/NewUser.js
@@ -10,26 +10,22 @@ import {
 import i18n from '../../translations/il18';
 
 export default class NewUser extends Component {
-    render() {
-        return (
-            <SafeAreaView style={styles.root}> 
-                <View style={styles.root}>
-                    <Text style={styles.advice}>{i18n.t('signin.newUser')}</Text>
-                    
-
-                </View>
-            </SafeAreaView>
-          );
-    }
+  render() {
+    return (
+      <SafeAreaView style={styles.root}>
+        <View style={styles.root}>
+          <Text style={styles.advice}>{i18n.t('signin.newUser')}</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
-
-const styles=StyleSheet.create({
-    root: {
-        flex:1,
-    },
-    advice: {
-        fontSize: 20,
-    }
-
-})
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  advice: {
+    fontSize: 20,
+  },
+});
