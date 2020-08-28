86504894d4e13c52fa6c7904814a1a50aac8e166
Jesus <Jesusreynaga96@gmail.com>
2020-08-28 00:44:34 -0500

    Fix the UI for new user

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import {Dropdown} from 'react-native-material-dropdown-v2';
 
 export default class Home extends Component {
   render() {
+    let data = [
+      {
+        value: 'Banana',
+      },
+      {
+        value: 'Mango',
+      },
+      {
+        value: 'Pear',
+      },
+    ];
     return (
       <View style={styles.root}>
         <Text> textInComponent </Text>
+        <Dropdown
+          value="Mexicana"
+          data={data}
+          lineWidth={0}
+          underlineColor="transparent"
+        />
       </View>
     );
   }
@@ -15,6 +33,5 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
   },
 });
--- src/screens/NewUser/NewUser.js
+++ src/screens/NewUser/NewUser.js
@@ -6,12 +6,27 @@ import {
   TextInput,
   TouchableOpacity,
   SafeAreaView,
+  Dimensions,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import i18n from '../../translations/il18';
-
+import {Dropdown} from 'react-native-material-dropdown-v2';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+const {height} = Dimensions.get('screen');
 export default class NewUser extends Component {
   render() {
+    let data = [
+      {
+        value: 'Banana',
+      },
+      {
+        value: 'Mango',
+      },
+      {
+        value: 'Pear',
+      },
+    ];
+
     return (
       <LinearGradient
         start={{x: 0, y: 1}}
@@ -20,74 +35,87 @@ export default class NewUser extends Component {
         style={{flex: 1}}>
         <SafeAreaView style={styles.root}>
           <View style={styles.root}>
-            <Text style={styles.advice}>{i18n.t('signin.newUser')}</Text>
-            <View style={{marginHorizontal: '10%', flex: 1}}>
-              <Text style={styles.labels}>{i18n.t('signin.name')}</Text>
-              <View style={styles.inputs}>
-                <TextInput
-                  returnKeyType="next"
-                  style={styles.textInputs}
-                  onSubmitEditing={() => {
-                    this.name.focus();
-                  }}
-                />
+            <KeyboardAwareScrollView>
+              <Text style={styles.advice}>{i18n.t('signin.newUser')}</Text>
+              <View style={{marginHorizontal: '10%', flex: 1}}>
+                <Text style={styles.labels}>{i18n.t('signin.name')}</Text>
+                <View style={styles.inputs}>
+                  <TextInput
+                    returnKeyType="next"
+                    style={styles.textInputs}
+                    onSubmitEditing={() => {
+                      this.name.focus();
+                    }}
+                  />
+                </View>
+                <Text style={styles.labels}>{i18n.t('signin.lastName')}</Text>
+                <View style={styles.inputs}>
+                  <TextInput
+                    ref={input => {
+                      this.name = input;
+                    }}
+                    returnKeyType="next"
+                    style={styles.textInputs}
+                    onSubmitEditing={() => {
+                      this.lastName.focus();
+                    }}
+                  />
+                </View>
+                <Text style={styles.labels}>{i18n.t('signin.email')}</Text>
+                <View style={styles.inputs}>
+                  <TextInput
+                    ref={input => {
+                      this.lastName = input;
+                    }}
+                    keyboardType="email-address"
+                    returnKeyType="next"
+                    style={styles.textInputs}
+                    onSubmitEditing={() => {
+                      this.password.focus();
+                    }}
+                  />
+                </View>
+                <Text style={styles.labels}>{i18n.t('signin.password')}</Text>
+                <View style={styles.inputs}>
+                  <TextInput
+                    ref={input => {
+                      this.password = input;
+                    }}
+                    secureTextEntry
+                    returnKeyType="next"
+                    style={styles.textInputs}
+                    onSubmitEditing={() => {
+                      this.countryInput.focus();
+                    }}
+                  />
+                </View>
+                <Text style={styles.labels}>
+                  {i18n.t('signin.nationality')}
+                </Text>
+                <View style={{...styles.inputs, paddingTop: 14}}>
+                  <Dropdown
+                    ref={country => {
+                      this.countryInput = country;
+                    }}
+                    data={data}
+                    fontSize={18}
+                    labelFontSize={18}
+                    underlineColor="transparent"
+                    value={'Mexicana'}
+                    onChangeText={text => {
+                      console.log(text);
+                    }}
+                  />
+                </View>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate('Home')}
+                  style={styles.btnSignUp}>
+                  <Text style={styles.labelBtn}>
+                    {i18n.t('signin.register')}
+                  </Text>
+                </TouchableOpacity>
               </View>
-              <Text style={styles.labels}>{i18n.t('signin.lastName')}</Text>
-              <View style={styles.inputs}>
-                <TextInput
-                  ref={input => {
-                    this.name = input;
-                  }}
-                  returnKeyType="next"
-                  style={styles.textInputs}
-                  onSubmitEditing={() => {
-                    this.lastName.focus();
-                  }}
-                />
-              </View>
-              <Text style={styles.labels}>{i18n.t('signin.email')}</Text>
-              <View style={styles.inputs}>
-                <TextInput
-                  ref={input => {
-                    this.lastName = input;
-                  }}
-                  keyboardType="email-address"
-                  returnKeyType="next"
-                  style={styles.textInputs}
-                  onSubmitEditing={() => {
-                    this.password.focus();
-                  }}
-                />
-              </View>
-              <Text style={styles.labels}>{i18n.t('signin.password')}</Text>
-              <View style={styles.inputs}>
-                <TextInput
-                  ref={input => {
-                    this.password = input;
-                  }}
-                  secureTextEntry
-                  returnKeyType="next"
-                  style={styles.textInputs}
-                  onSubmitEditing={() => {
-                    // this.pss.focus();
-                  }}
-                />
-              </View>
-              <Text style={styles.labels}>{i18n.t('signin.nationality')}</Text>
-              <View style={styles.inputs}>
-                <TextInput
-                  returnKeyType="next"
-                  style={styles.textInputs}
-                  onSubmitEditing={() => {}}
-                />
-              </View>
-              <View style={{flex: 0.5}} />
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Home')}
-                style={styles.btnSignUp}>
-                <Text style={styles.labelBtn}>{i18n.t('signin.register')}</Text>
-              </TouchableOpacity>
-            </View>
+            </KeyboardAwareScrollView>
           </View>
         </SafeAreaView>
       </LinearGradient>
@@ -101,10 +129,11 @@ const styles = StyleSheet.create({
   },
   advice: {
     fontSize: 30,
-    color: 'black',
+    color: 'white',
     textAlign: 'center',
     marginBottom: '10%',
     fontWeight: 'bold',
+    marginTop: height > 800 ? '15%' : '5%',
   },
   labels: {
     fontSize: 23,
@@ -116,7 +145,7 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     backgroundColor: '#EAE5D2',
     justifyContent: 'center',
-    marginBottom: 20,
+    marginBottom: 15,
   },
   textInputs: {
     fontSize: 18,
@@ -131,6 +160,7 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 15,
   },
   labelBtn: {
     fontSize: 23,
